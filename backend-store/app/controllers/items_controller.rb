0d220899a5f1c91f65d437502cc9c697a3a5f798
Charlotte Neff <charlotte.neff@gmail.com>
2021-03-11 18:04:46 -0500

    commit

--- backend-store/app/controllers/items_controller.rb
+++ backend-store/app/controllers/items_controller.rb
@@ -1,15 +1,42 @@
 class ItemsController < ApplicationController
 
     def index
-        items = Item.all 
-        options = {include: [:category]}
-        render json: ItemSerializer.new(items, options)
+        items = Item.all
+        render json: ItemSerializer.new(items)
     end
 
-    def show 
+    def show
         item = Item.find(params[:id])
-        options = {include: [:category]}
-        render json: ItemSerializer.new(item, options)
-    end 
+        render json: ItemSerializer.new(item)
+    end
+
+    def create
+        item = Item.new(item_params)
+        item.category_id = 1
+        if item.save
+            render json: ItemSerializer.new(item)
+        else
+            render json: {error: 'could not be created'}
+        end
+    end
+
+    def destroy
+        item = Item.find(params[:id])
+        item.destroy
+        render json: {message: "Successfully deleted #{item.name}!"}
+    end
+
+    def update
+        item = Item.find(params[:id])
+        item.update(item_params)
+        render json: ItemSerializer.new(item)
+    end
+
+    private
+
+    def item_params
+        params.require(:item).permit(:name, :description, :price)
+    end
+
 
 end
--- frontend/index.js
+++ frontend/index.js
@@ -1,9 +1,13 @@
 const itemList = document.getElementById('item-list')
+const itemForm = document.getElementById('item-form')
+const itemPrice = document.getElementById('item-price')
+const itemDescription = document.getElementById('item-description')
+const itemName = document.getElementById('item-name')
 
 function fetchItems(){
     fetch('http://localhost:3000/items')
     .then(res => res.json())
-    .then(data => addItems(data))
+    .then(addItems)
 }
 
 function addItems(response){
@@ -20,10 +24,138 @@ function addItemToDom(item){
         <strong class="name">${item.attributes.name}</strong>:
         <span class="description">${item.attributes.description}</span>
         </li>
+        <button class="delete" data-id="${item.id}">Delete</button>
+        <button class="update" data-id="${item.id}">Update</button>
         </div>`
 }
 
+function handleFormSubmit(e){
+    e.preventDefault()
+
+    let newItemObj = {
+        name: itemName.value,
+        description: itemDescription.value,
+        price: itemPrice.value
+    }
+
+    let configObj = {
+        method: 'POST',
+        headers: {
+            "Content-Type": "application/json",
+            Accept: "application/json"
+        },
+        body: JSON.stringify(newItemObj)
+    }
+
+    //pessimistic rendering
+    fetch('http://localhost:3000/items', configObj)
+    .then(res => res.json())
+    .then(json => {
+        addItemToDom(json.data)
+    })
+
+    itemForm.reset()
+}
+
+function addUpdateItemFields(itemId){
+    let item = document.querySelector(`#item-${itemId} li`)
+    let price = item.querySelector('.price').innerText
+    let description = item.querySelector('.description').innerText
+    let name = item.querySelector('strong').innerText
+
+
+    let updateForm = `
+    <input type="number" value="${price}" name="price" id="update-price-${itemId}" min="0" step=".01">
+    <input type="text" name="name" value="${name}" id="update-name-${itemId}">
+    <input type="text" name="description" value="${description}" id="update-description-${itemId}">
+    `
+
+    let formDiv = document.createElement('div')
+    formDiv.id = `update-form-${itemId}`
+    formDiv.innerHTML = updateForm
+    item.append(formDiv)
+}
+
+function sendPatchRequest(itemId){
+
+    const updateItemPrice = document.getElementById(`update-price-${itemId}`)
+    const updateItemDescription = document.getElementById(`update-description-${itemId}`)
+    const updateItemName = document.getElementById(`update-name-${itemId}`)
+
+    let itemObj = {
+        name: updateItemName.value,
+        description: updateItemDescription.value,
+        price: updateItemPrice.value
+    }
+
+    let configObj = {
+        method: 'PUT',
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json"
+        },
+        body: JSON.stringify(itemObj)
+    }
+
+    fetch(`http://localhost:3000/items/${itemId}`, configObj)
+    .then(res => res.json())
+    .then(response => updateItemOnDom(response.data))
+    // remove form
+
+    let form = document.getElementById(`update-form-${itemId}`)
+    form.remove()
+}
+
+function updateItemOnDom(item){
+    let liItem = document.querySelector(`#item-${item.id} li`)
+    liItem.querySelector('.price').innerText = item.attributes.price
+    liItem.querySelector('.description').innerText = item.attributes.description
+    liItem.querySelector('strong').innerText = item.attributes.name
+}
+
+function deleteItem(id){
+// remove from db
+    let configObj = {
+        method: 'DELETE',
+        headers: {
+            "Content-Type": "application/json",
+            Accept: "application/json"
+        }
+    }
+
+    fetch(`http://localhost:3000/items/${id}`, configObj)
+    .then(res => res.json())
+    .then(json => {
+        alert(json.message)
+    })
+
+
+// remove from dom
+// optimistic rendering
+    let item = document.getElementById(`item-${id}`)
+    item.remove()
+}
+
+function handleListClick(e){
+   if (e.target.className === "delete"){
+       let id = e.target.dataset.id
+        deleteItem(id)
+   } else if(e.target.className === 'update'){
+        let itemId = e.target.dataset.id
+        e.target.className = "save"
+        e.target.innerText = "Save"
+        addUpdateItemFields(itemId)
+    } else if(e.target.className === 'save'){
+        let itemId = e.target.dataset.id
+        e.target.className = "update"
+        e.target.innerText = "Update"
+        sendPatchRequest(itemId)
+    }
+}
+
+
 document.addEventListener('DOMContentLoaded', () => {
     fetchItems()
-
+    itemForm.addEventListener('submit', handleFormSubmit)
+    itemList.addEventListener('click', handleListClick)
 })
